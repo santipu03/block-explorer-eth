547299bae4a707e6dd59e7f9fa1adcefe60dd736
santipu <santipujolsau@gmail.com>
2022-12-21 10:55:58 +0100

    set null address when the tx is sent to the null adress

--- src/components/LatestTransactions.jsx
+++ src/components/LatestTransactions.jsx
@@ -1,16 +1,23 @@
-function LatestTransactions(latestTx) {
+function LatestTransactions(latestTxs) {
   return (
     <div className="tx-container">
       <div className="card-header">Latest Transactions</div>
       <div className="row-container">
-        {console.log(latestTx.latestTx)}
-        {latestTx.latestTx.map((tx, i) => {
+        {latestTxs.latestTxs.map((tx, i) => {
           return (
             <div className="row" key={i}>
               <div>{tx.hash.substring(0, 11)}...</div>
               <div>
                 <div>From {tx.from.substring(0, 11)}...</div>
-                <div>To {tx.to.substring(0, 11)}...</div>
+                <div>
+                  To{" "}
+                  {tx.to === null ? (
+                    <div>0x000000000</div>
+                  ) : (
+                    tx.to.substring(0, 11)
+                  )}
+                  ...
+                </div>
               </div>
               <div>{(tx.value.toString() / 10 ** 18).toFixed(4)} ETH</div>
             </div>
